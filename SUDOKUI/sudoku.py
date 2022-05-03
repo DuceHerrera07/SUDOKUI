6d221a4f3c80b5a29cab28b59207d5a4cee5650f
DuceHerrera07 <95140708+DuceHerrera07@users.noreply.github.com>
2022-05-03 15:46:42 -0600

    Update sudoku.py

--- SUDOKUI/sudoku.py
+++ SUDOKUI/sudoku.py
@@ -1,6 +1,8 @@
 #Importar libreria
+import tkinter
 import pygame#libreria para hacer juegos en dos dimensiones
 import tkinter as tk #blioteca grafica (GUI)
+import tkinter.font as tkFont#libreria del texto y abrevacion del llamdo de la funcion
 from tkinter import*#para gregar la funcion de texto
 #Inicio de pygame
 pygame.init()
@@ -21,41 +23,59 @@ numeros_del_tablero=[
 pantalla = pygame.display.set_mode((900,810))#ancho y altura
 #fondo de ventana
 pantalla.fill("white")
-
 #icono de ventana pygame
 icono=pygame.image.load ("sudoku_Icono.jpg")
 pygame.display.set_icon(icono)
 #titulo de la ventana
 pygame.display.set_caption("Sudoku")
-
 #tipo de fuente para texto
 fuente_texto=pygame.font.SysFont(None,70)
 #actualizar pantalla
 reloj=pygame.time.Clock()
+
 def ventana2():
         ven = tk.Tk()
         #ancho y largo (orden de geometry)
-        ven.geometry('1500x910')#dimension de ventana
+        ven.geometry('1300x720')#dimension de ventana
         ven.title('Reglas de sodoku clásico')#titulo
         ven.iconbitmap(r"instruccion.ico")#icono
-        ven.configure(bg="black")#color de fondo
-        inst=Label(ven,text="Instrucción",bg="white")
-        inf=Label(ven,text="Al hacer clic en un boton del tablero",bg="white")
-        inf1=Label(ven,text="podra decidir las cordenas de sus",bg="white")
-        inf2=Label(ven,text="barcos, cuando estos esten asignados",bg="white")
-        inf3=Label(ven,text="correctamente se mostraran de color marron.",bg="white")
-        m=Label(ven,text="Cierre esta ventana para continuar",bg="white")
-        #para que se muestren en pantalla las variables anteriores que son str, columna y fila en la que apareceran
-        inst.grid(column=11,row=2)
-        inf.grid(column=11,row=3)
-        inf1.grid(column=11,row=4)
-        inf2.grid(column=11,row=5)
-        inf3.grid(column=11,row=6)
-        m.grid(column=11,row=7)
-        salir=tk.Button(ven,text="Entendido")
+        ven.configure(bg="white")#color de fondo
+        #imagen-formato de imagen
+        imagen=tkinter.PhotoImage(file="tkinter_imagen_N.gif")#variable que guarda la imagen
+        sobre_ventana=tkinter.Label(ven,image=imagen).place(x=0,y=0)#activa la imagen y posiciona en cordenadas especificas
+        ven.configure(bg="white")#color de fondo
+        #funcion para agregar estilo de fuente y tamaño
+        estilo_fuente=tkFont.Font(family="Cascadia Mono SemiLight",size=20)
+        fuente=tkFont.Font(family="Berlin Sans FB Demi",size=15)#para el botón
+        #inicia el los textos que se mostraran sobre la ventana de tkinter
+        texto=Label(ven,text="Un rompecabezas en Sudoku comienza con una",font=estilo_fuente,bg="white")
+        texto1=Label(ven,text="cuadricula en el cual algunos de los números ya,",font=estilo_fuente,bg="white")
+        texto2=Label(ven,text="están en su lugar en dependencia de la dificultad del juego. Se completa",font=estilo_fuente,bg="white")
+        texto3=Label(ven,text="de un rompecabezas cuando cada número del 1 al 9 ",font=estilo_fuente,bg="white")
+        texto4=Label(ven,text="aparecen solamente una vez en cada una de las 9",font=estilo_fuente,bg="white")
+        texto5=Label(ven,text="filas,",font=estilo_fuente,bg="white",foreground="yellow")
+        texto6=Label(ven,text="columnas",font=estilo_fuente,bg="white",foreground="red")
+        texto7=Label(ven,text="y",font=estilo_fuente,bg="white")
+        texto8=Label(ven,text="celdas.",font=estilo_fuente,bg="white",foreground="green")
+        texto9=Label(ven,text="Analice la cuadrícula para encontar los números",font=estilo_fuente,bg="white")
+        texto10=Label(ven,text="que pudiera encajar en cada celda del tablero.",font=estilo_fuente,bg="white")
+        #para que se muestren en pantalla las variables anteriores que son str, x & y en la que apareceran
+        texto.place(x=500,y=5)#x= lo corre hacia la derecha e izquierda
+        texto1.place(x=500,y=40)#y= lo corre hacia abajo y arriba
+        texto2.place(x=500,y=80)#+40 en y=
+        texto3.place(x=500,y=120)
+        texto4.place(x=500,y=160)
+        texto5.place(x=500,y=200)#yellow
+        texto6.place(x=600,y=200)#red
+        texto7.place(x=740,y=200)#y
+        texto8.place(x=770,y=200)#green
+        texto9.place(x=500,y=280)
+        texto10.place(x=500,y=320)
+        #crear boton que cierre la ventana de tkinter
+        salir=tk.Button(text="¡Entendido!",command=ven.destroy,height=3,width=15,font=fuente,bg="white").place(x=750,y=400)
         ven.mainloop()
-    
-#condiciones y funciones
+        
+#condiciones y funciones para la ventana del juego
 def dibujo_tablero(): 
     color_bordes=pygame.Color("black")
     pygame.draw.rect(pantalla,color_bordes,pygame.Rect(15,15,785,785),10)#marco del tablero bordes externos
@@ -80,10 +100,12 @@ def numeros_sobre_tablero_funcion():
             pantalla.blit(texto_numeros,pygame.Vector2((columna*88) + Compensar,(fila * 88) + Compensar))#para mostrar los numero sobre el tablero
             columna += 1 
         fila += 1
+        
 #funciones  
 ventana2()        
 dibujo_tablero()
 numeros_sobre_tablero_funcion()   
+
 #Bandera      
 bandera=True
 #funciones de mouse y teclado sobre el tablero
@@ -126,38 +148,11 @@ while bandera:
             if event.key==pygame.K_9:
                 key=9
                 print("Soy la tecla 9")
-            #para eliminar, aun no funciona del todo.
-            if event.key==pygame.K_DELETE:
-                posicion.clear()
-                key=None
-    #editado por wilson Armando
-    #cada fila debe contener los digitos de 1 al 9 sin repeticdión 
-    def chequeo_filas(self,chequear_lista="todo el tablero");
-     if chequear_lista =="todo el tablero":
-         chequear_lista == self.tablero
-     for fila in self.tablero
-        for elemento in fila:
-            if elemento != "0":
-                assert fila.count(elemento) ==1, " tablero no valido"
-                
-                
-    def chequeo_columnas (self):
-        for columna_index in range(0.9):
-            for row_index in range(0.9):
-                self.lista_invertida.append(self.tablero[row_index][columna_index])
- 
- 
-        self.chequeo_filas([self.lista_invertida])  
-        self.lista_invertida.clear() 
-    
-
-                                         
+                                                
         #rapidez de actualizacion de pantalla
         reloj.tick(60)
         pygame.display.update()#activa todo lo que este sobre pantalla sin ello no se ejecuta
         pygame.display.flip()#actualiza la superficie de visualizacion completa
-        chequeo_filas() # estoy llamando a la funcion de rrepetición de filas 
-        chequeo_columnas() #llamando a la funcion de repeticion de  columnas 
 
 #IDLE_cerrar ventana    
 pygame.quit()
